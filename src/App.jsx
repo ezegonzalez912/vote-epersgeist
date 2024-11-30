4f32dbea89a8751382f160ee4cfb3863d2eb70ab
Ezequiel Gonzalez <83894961+ezegonzalez912@users.noreply.github.com>
2024-11-30 12:37:16 -0300

    vote group

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { createClient } from "@supabase/supabase-js";
+import { Evaluate } from "./components/Evaluate";
 
 const api_key = import.meta.env.VITE_SUPABASE_API_KEY;
 
@@ -10,9 +11,13 @@ const supabase = createClient(
 );
 
 function App() {
-  const [name, setName] = useState(null);
+  const [user, setUser] = useState(null);
   const [vote, setVote] = useState(null);
   const [node, setNode] = useState(1);
+  const [viewMore, setViewMore] = useState(false);
+  const [saved, setSaved] = useState(false);
+
+  const { name } = user || {};
 
   const handleVote = async (option) => {
     setVote(option);
@@ -26,6 +31,38 @@ function App() {
     }
   };
 
+  const handleEvaluate = async () => {
+    const { data } = await supabase.from("votes").select();
+    console.log(data);
+
+    const result = Object.values(
+      data.reduce((acc, { node, vote }) => {
+        const key = node;
+        if (!acc[key]) acc[key] = { node, votes: {} };
+        const voteKey = vote.toLowerCase();
+        acc[key].votes[voteKey] = (acc[key].votes[voteKey] || 0) + 1;
+        return acc;
+      }, {})
+    ).map(({ node, votes }) => {
+      const winner = Object.keys(votes).reduce((a, b) =>
+        votes[a] > votes[b] ? a : b
+      );
+      return { node, winner };
+    });
+
+    console.log(result, user);
+
+    if (result) {
+      const { winner } = result[result.length - 1];
+      setSaved({ status: winner !== user.groupId });
+      setTimeout(() => {
+        setVote(null);
+        setNode((prev) => prev + 1);
+        setSaved(null);
+      }, 15000);
+    }
+  };
+
   const nextQuestion = () => {
     setVote(null);
     setNode((prev) => prev + 1);
@@ -33,68 +70,122 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setName(e.target["name"].value);
+    setUser({
+      name: e.target["name"].value,
+      groupId: e.target["group"].value,
+    });
   };
 
-  if(node === 6) {
-    return <div>
-      <h1>👻</h1>
-      <h2>¡{name}, gracias por participar! Esperemos lo hayas disfrutado.</h2>
-      <p>¡Contamos con tu voto al final!</p>
-      <p>Att: Persistir a lo gradle</p>
-    </div>
-  }
-
-  if (!name) {
+  if (node === 6) {
     return (
       <div>
+        <h1>👻</h1>
+        <h2>¡{name}, gracias por participar! Esperemos lo hayas disfrutado.</h2>
+        <p>¡Contamos con tu voto al final!</p>
+        <p>Att: Persistir a lo gradle</p>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return (
+      <div className="initial-page">
         <h1>¡Hola!</h1>
         <p>
-          Somos persistir a lo gradle, ingresa tu nombre y ayudanos a decidir el
+          Somos persistir a lo gradle, ingresa tu nombre y ayúdanos a decidir él
           final
         </p>
         <form onSubmit={handleSubmit}>
           <input id="name" placeholder="Nombre" />
+          <select id="group">
+            <option value="a">Profes 👨‍🏫</option>
+            <option value="b">The EPERSstrikes Back 💫</option>
+            <option value="c">Los Ghost Bugsters 🔥</option>
+            <option value="d">MancosDB ⌛</option>
+            <option value="e">NullPointer Kings ⏩</option>
+            <option value="f">Los Angeles de Guido ☁️</option>
+            <option value="g">Tamos Redis 🫂</option>
+          </select>
           <button type="onSubmit">Listo!</button>
         </form>
       </div>
     );
   }
 
+  if (saved) {
+    return <Evaluate saved={saved.status} />;
+  }
+
   return (
     <>
       <h1 className="title">{name}, elige una opción 👻</h1>
-      <p>Nota: Vota solo por las opciones disponibles en el proyector y solo cuando demos el GO!</p>
+      <p>
+        Nota: Vota solo por las opciones disponibles en el proyector y solo
+        cuando demos el GO!
+      </p>
       <div className="buttons">
         <button
           onClick={() => handleVote("A")}
           disabled={vote}
-          className={vote === "A" ? `button-vote` : ""}
+          className="button-vote"
         >
           Opción A
         </button>
         <button
           onClick={() => handleVote("B")}
           disabled={vote}
-          className={vote === "B" ? `button-vote` : ""}
+          className="button-vote"
         >
           Opción B
         </button>
-        <button
-          onClick={() => handleVote("C")}
-          disabled={vote}
-          className={vote === "C" ? `button-vote` : ""}
-        >
-          Opción C
+        {viewMore && (
+          <>
+            <button
+              onClick={() => handleVote("C")}
+              disabled={vote}
+              className="button-vote"
+            >
+              Opción C
+            </button>
+            <button
+              onClick={() => handleVote("D")}
+              disabled={vote}
+              className="button-vote"
+            >
+              Opción D
+            </button>
+            <button
+              onClick={() => handleVote("E")}
+              disabled={vote}
+              className="button-vote"
+            >
+              Opción E
+            </button>
+            <button
+              onClick={() => handleVote("F")}
+              disabled={vote}
+              className="button-vote"
+            >
+              Opción F
+            </button>
+            <button
+              onClick={() => handleVote("G")}
+              disabled={vote}
+              className="button-vote"
+            >
+              Opción G
+            </button>
+          </>
+        )}
+        <button onClick={() => setViewMore((prev) => !prev)}>
+          {viewMore ? "Menos" : "Mas"} opciones...
         </button>
-        <button
-          onClick={() => handleVote("D")}
-          disabled={vote}
-          className={vote === "D" ? `button-vote` : ""}
-        >
-          Opción D
-        </button>
-        {vote && <button onClick={nextQuestion}>Siguiente pregunta</button>}
+        {vote && (
+          <section className="next-question">
+            <button onClick={handleEvaluate}>? ? ? ? ?</button>
+            <button onClick={nextQuestion}>Siguiente pregunta</button>
+          </section>
+        )}
       </div>
       <p className="read-the-docs">Desarrollado por Persistir a lo gradle</p>
     </>
---
+++ src/components/Evaluate.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export const Evaluate = ({ saved }) => {    
+  const [finish, setFinish] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setFinish(true), 5000);
+  }, []);
+
+  if (!finish) return <p>Analizando resultados</p>;
+
+  return (
+    <div className="container">
+      <div className={`${saved ? "circle saved" : "circle kill"}`}>
+        <p className="text-saved">
+          {saved ? "Te salvaste" : "Te sacrificaron!!!"}
+        </p>
+      </div>
+    </div>
+  );
+};
